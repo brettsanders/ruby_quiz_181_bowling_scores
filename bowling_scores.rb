552510419b51ea7eda1b5e52df7192de89e481fc
Brett Sanders <brettrsanders@gmail.com>
2013-11-12 23:33:09 -0600

    initial commit

---
+++ bowling_scores.rb
@@ -0,0 +1,25 @@
+# 
+# Bowling_scores.rb John 6 2 7 1 10 9 0 8 2 10 10 3 5 7 2 5 5 8
+# Your should tally the per-frame scores and generate output in table form, such as:
+# 
+# John's final score: 140
+# 
+# Frame     Roll  Roll    Score
+#    1        6     2        8
+#    2        7     1       16
+#    3        X             35
+#    4        9     -       44
+#    5        8     /       64
+#    6        X             87
+#    7        X            105
+#    8        3     5      113
+#    9        7     2      122
+#   10        5     /      140
+#    *        8
+
+
+class Frame
+end
+
+class DisplayScore
+end
